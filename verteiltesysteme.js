c6ba85d4f2b90bd3d1aaf547f0fd270d6743499b
Jobee0602 <burkert.benny@gmx.de>
2020-02-18 17:14:49 +0100

    Delete verteiltesysteme.js

--- verteiltesysteme.js
+++
@@ -1,9 +0,0 @@
-var eingabe = document.querySelector('#eingabe');
-
-document.getElementById("bestaetigen").addEventListener("click", wertauslesen);
-
-function wertauslesen() {
-    var wert = eingabe.value;
-    alert(wert);
-    eingabe.value = null;
-};
