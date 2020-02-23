1aae381d214155d9a293793eae56dabc750ca227
Benny Burkert <burkert.benny@gmx.de>
2020-02-23 18:20:34 +0100

    basic query added

--- verteiltesysteme.py
+++ verteiltesysteme.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
@@ -9,7 +10,7 @@ app.debug = True
 db = SQLAlchemy(app)
 
 @app.route('/', methods=['GET', 'POST']) #allow GET & Post requests
-def test():
+def enter_client():
     if request.method == 'POST': #called by  POST request | first and last name mandatory
         req_data = request.get_json()
 
@@ -26,18 +27,38 @@ def test():
 #call: /query?name=<value>&mandatory=<value>
 @app.route('/query')
 def test_query():
-    name = request.args.get('name')
-    mandatory = request.args['mandatory'] #no .get -> Error 400 for none value
+    first = request.args.get('first')
+    last = request.args.get('last')
+    company = request.args.get('company')
+    mail = request.args.get('mail')
+    tel = request.args.get('tel')
 
-    return '''<h1>The name is: {}</h1>
-              <h2>The mandatory value is: {}</h2>'''.format(name, mandatory)
+    sql = "SELECT * FROM client WHERE "
+    sql += "first_name Like '" + first + "'" if first is not None else ""
 
+    results = db.engine.execute(text(sql))
+    names = []
+    for row in results:
+        names.append(list(row._row))
+        print(type(names[0]))
+
+    print (names[0][3])
+    return '''<h1>{}</h1>
+              <p>Company: {}</p>
+              <p>E-Mail: {}</p>
+              <p>Tel.: {}</p>'''.format(names[0][1] + " " + names[0][2], names[0][3], names[0][4], names[0][5])
+
+@app.route('/getclient', methods=['POST', 'GET'])
+def get_client():
+    if request.method == 'POST':
+        req_date = request.get_json()
+    return None
 
 #Database Stuff
 class Client(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(80), unique=True)
-    last_name = db.Column(db.String(80), unique=True, nullable=False)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80), nullable=False)
     company = db.Column(db.String(80))
     mail = db.Column(db.String(120))
     tel = db.Column(db.Integer, unique=True)
